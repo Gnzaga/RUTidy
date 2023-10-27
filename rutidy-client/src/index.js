734f7846f90e4b13bb8a705cd04d117b43964a36
Aidan Cronin <104910154+AidanJCronin@users.noreply.github.com>
2023-10-27 14:52:23 -0400

    Add files via upload

--- rutidy-client/src/index.js
+++ rutidy-client/src/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Login from "./js/Login";
 import Signup from "./js/Signup";
+import Home from "./js/Home";
 import GroupDetails from "./js/GroupDetails";
+import Profile from "./js/Profile";
+import CreateGroup from "./js/CreateGroup";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 
@@ -12,7 +15,10 @@ root.render(
     <Routes>
       <Route path = "/" element = {<Login />}></Route>
       <Route path = "/signup" element = {<Signup />}></Route>
-      <Route path = "/groupdetails" element = {<GroupDetails />}></Route>
+      <Route path = "/home" element = {<Home/>}></Route>
+      <Route path = "/groupdetails" element = {<GroupDetails/>}></Route>
+      <Route path = "/edit/profile" element = {<Profile/>}></Route>
+      <Route path = "/create/group" element = {<CreateGroup/>}></Route>
     </Routes>
   </BrowserRouter>
 );
