ca9f9e3531a3988865ed4c9d137aeacd0519038e
chrisosma24 <101012681+chrisosma24@users.noreply.github.com>
2023-11-29 17:13:41 -0500

    Update index.js

--- rutidy-client/src/index.js
+++ rutidy-client/src/index.js
@@ -19,18 +19,18 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <Routes>
-      {/*<Route path = "/" element = {<Login />}></Route>
-      <Route path = "/signup" element = {<Signup />}></Route>*/}
+      <Route path = "/" element = {<Login />}></Route>
+      <Route path = "/signup" element = {<Signup />}></Route>
       <Route path = "/home" element = {<Home/>}></Route>
-      {/*<Route path = "/profile" element = {<Profile />}></Route>
-      <Route path = "/profile/edit" element = {<EditProfile />}></Route>*/}
-      {/*<Route path = "/groupdetails/:groupID" element = {<GroupDetails/>}></Route>*/}
-      {/*<Route path = "/chores/:groupID" element = {<UserChores/>}></Route>*/} 
-      {/*<Route path = "/edit/task/:groupID/:taskID" element = {<EditTask/>}></Route>*/}
-      {/*<Route path = "/admin/tasks/:groupID" element = {<AdminTasks />}></Route>*/}
-      {/*<Route path = "/create/task/:groupID" element = {<CreateTask/>}></Route>
-      <Route path = "/create/nonscheduledtask/:groupID" element = {<CreateNSTask/>}></Route>*/}
-      {/*<Route path = "/create/group" element = {<CreateGroup/>}></Route>*/}
+      <Route path = "/profile" element = {<Profile />}></Route>
+      <Route path = "/profile/edit" element = {<EditProfile />}></Route>
+      <Route path = "/groupdetails/:groupID" element = {<GroupDetails/>}></Route>
+      <Route path = "/chores/:groupID" element = {<UserChores/>}></Route>
+      <Route path = "/edit/task/:groupID/:taskID" element = {<EditTask/>}></Route>
+      <Route path = "/admin/tasks/:groupID" element = {<AdminTasks />}></Route>
+      <Route path = "/create/task/:groupID" element = {<CreateTask/>}></Route>
+      <Route path = "/create/nonscheduledtask/:groupID" element = {<CreateNSTask/>}></Route>
+      {<Route path = "/create/group" element = {<CreateGroup/>}></Route>
     </Routes>
   </BrowserRouter>
 );
